74ee9df147242d957d0bda03566c88e8362b136d
unknown <skrillathegoon@gmail.com>
2021-12-11 04:13:54 -0500

    Created views and linked views navigation through HomeController, AccountViewModel responsible for login and registration

--- BuyIt/Controllers/HomeController.cs
+++ BuyIt/Controllers/HomeController.cs
@@ -18,17 +18,31 @@ namespace BuyIt.Controllers
             _logger = logger;
         }
 
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult Payment()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public ActionResult Registration()
+        {
+            return View();
+        }
+        public IActionResult ShoppingCart()
+        {
+            return View();
+        }
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
