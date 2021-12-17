ed4dab8c140000ef0d0cdde60ae3c56847afa507
unknown <skrillathegoon@gmail.com>
2021-12-17 18:16:21 -0500

    Added WebApi files back, found in BuyIt

--- BuyIt/script.js
+++ BuyIt/script.js
@@ -43,21 +43,4 @@ document.getElementById('login-submit').addEventListener('click', () => {
     
 })
 
-//Event Listener for on Click of Sign Up Button 
-document.getElementById('signup-submit').addEventListener('click', () => {
-    var obj = {}; // Creating user object
-    obj.email = document.getElementById("exampleInputEmail1").value
-    obj.password = document.getElementById("exampleInputEmail2").value // Extracting form values
-    fetch('http://localhost:5000/api/signup', { // When user signs up POST user and add to a list of Users 
-        method: "POST", //
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify(obj)
-    })
-    then(response => response.json())
-        .then(data => {
-            document.getElementByIdById("exampleInputEmail1").value = "";
-            document.getElementByIdById("exampleInputEmail2").value = "";
-        });
-})
+//  
