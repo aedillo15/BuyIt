6b81696fa0994d310200158e214808b26c7a91a0
jatinsharma <sharm212@sheridancollege.ca>
2021-12-17 18:18:28 -0500

    cleaned

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){ 
 
     //Switch Page from login to signup
@@ -38,26 +37,3 @@ $(document).ready(function(){
   //  }
  
  });
-//Event Listener for on Click of Login Button 
-document.getElementById('login-submit').addEventListener('click', () => {
-    
-})
-
-//Event Listener for on Click of Sign Up Button 
-document.getElementById('signup-submit').addEventListener('click', () => {
-    var obj = {}; // Creating user object
-    obj.email = document.getElementById("exampleInputEmail1").value
-    obj.password = document.getElementById("exampleInputEmail2").value // Extracting form values
-    fetch('http://localhost:5000/api/signup', { // When user signs up POST user and add to a list of Users 
-        method: "POST", //
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify(obj)
-    })
-    then(response => response.json())
-        .then(data => {
-            document.getElementByIdById("exampleInputEmail1").value = "";
-            document.getElementByIdById("exampleInputEmail2").value = "";
-        });
-})
