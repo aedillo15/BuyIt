87128375ff185e62bc6df9324a030ad07f7031e5
unknown <skrillathegoon@gmail.com>
2021-12-18 01:12:11 -0500

    Controller updated

--- BuyIt/Models/Identity/TokenService.cs
+++ BuyIt/Models/Identity/TokenService.cs
@@ -1,35 +1,76 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using Microsoft.IdentityModel.Tokens;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.IdentityModel.Tokens.Jwt;
+using BuyIt.Models;
+using BuyIt.Models.DTO;
+using BuyIt.Models.Entities;
+using BuyIt.Models.Identity;
+using BuyIt.Models.Persistence;
+using BuyIt.Models.Responses;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-namespace BuyIt.Models.Identity
+
+namespace BuyIt.Entities.Controllers
 {
-    public class TokenService
+    [Route("BuyIt/[controller]")]
+    public class UsersController : ControllerBase
     {
-        public string CreateToken(AppUser user)
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly TokenService _tokenService;
+        //todo: inject the userContext
+        public UsersController(
+            UserManager<AppUser> userManager,
+            SignInManager<AppUser> signInManager,
+            TokenService tokenService
+
+            )
         {
-                var claims = new List<Claim> {
-                    new Claim(ClaimTypes.NameIdentifier, user.Id),
-                    new Claim(ClaimTypes.Email, user.Email),
-                };
-
-                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("This is my super duper key"));
-                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-                var tokenDescriptor = new SecurityTokenDescriptor
-                {
-                    Subject = new ClaimsIdentity(claims),
-                    Expires = DateTime.Now.AddDays(7),
-                    SigningCredentials = creds
-                };
-
-                var tokenHandler = new JwtSecurityTokenHandler();
-                var token = tokenHandler.CreateToken(tokenDescriptor);
-                return tokenHandler.WriteToken(token);
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _tokenService = tokenService;
         }
+        
+        [HttpPost("SignUp")] // POST : /Users/SignUp
+        public async Task<IActionResult> Post([FromBody] SignUpDto signUpDto)
+        {
+            var result = await _userManager.CreateAsync(new AppUser
+            {
+                Email = signUpDto.Email,
+                UserName = signUpDto.UserName
+            }, signUpDto.Password);
+
+            if (result.Succeeded)
+                return Ok();
+
+            return BadRequest(result.Errors);
+
+        }
+
+        [HttpPost("Login")] // GET : /Users/Login
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            if (user == null)
+                return Unauthorized();
+
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, loginDto.Password, false);
+
+
+            if (!result.Succeeded)
+                return Unauthorized();
+
+            return Ok(new UserDto
+            {
+                Token = _tokenService.CreateToken(user)
+            });
+
+        }
+        
     }
 }
