3f1dda5a8125a33b8d8dd53369f34b9ad4319583
unknown <skrillathegoon@gmail.com>
2021-12-17 20:30:36 -0500

    Created Models folder

--- BuyIt/Controllers/UsersController.cs
+++ BuyIt/Controllers/UsersController.cs
@@ -3,18 +3,70 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using API.Models;
-//using API.Models.DTOs;
-using API.Models.Entities;
+using BuyIt.Models;
+//using BuyIt.Models.DTOs;
+using BuyIt.Models;
 //using API.Models.Responses;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 
-namespace API.Controllers
+namespace BuyIt.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("BuyIt/[controller]")]
     public class UsersController : ControllerBase
     {
+        private readonly UserContext _context;
+        //todo: inject the userContext
+        public UsersController(UserContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost] 
+        public async Task<IActionResult> CreateUser (User user)
+        {
+            await _context.users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return Ok(user);
+        }
+
+        //GET /BuyIt/Users/{id}
+        [HttpGet("{id}")]
+        // [Route("{id}")]
+        // [Route("getPerson/{id}")]
+        public async Task<IActionResult> GetUser(string emailAddress, string password)
+        {
+            try
+            {
+                var user = await _context.Persons.FindAsync();
+                // var list = new List<Person>();
+                // var response = 
+
+                if (user == null)
+                    return NotFound();
+                var userDto = new UserDTO
+                {
+                    Id = person.Id,
+                    Email = person.Email,
+                    Password = person.Password
+                };
+
+                return Ok(new Response<UserDTO>(userDto));
+            }
+            catch (System.Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        //POST : /BuyIt/
+        [HttpPost]
+        public async Task<IActionResult> PostArray([FromBody] User[] data)
+        {
+            await _context.users.AddRangeAsync(data);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
     }
-    }
+}
--- BuyIt/Startup.cs
+++ BuyIt/Startup.cs
@@ -1,15 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using API.Models.Identity;
+using API.Models.Persistence;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+
 
 namespace BuyIt
 {
@@ -27,10 +29,18 @@ namespace BuyIt
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "BuyIt", Version = "v1" });
+
+            services.AddDbContext<UserContext>(options => options.UseInMemoryDatabase("buyItDatabase"));
+            
+            service.AddCors(options => {
+                options.AddPolicy("AllowAllPolicy", policy => {
+                    policy.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod();
+                });
             });
+            // services.AddSwaggerGen(c =>
+            // {
+            //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "BuyIt", Version = "v1" });
+            // });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,11 +49,12 @@ namespace BuyIt
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BuyIt v1"));
+                // app.UseSwagger();
+                // app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BuyIt v1"));
             }
 
             app.UseRouting();
+            app.UseCor("AllowAllPolicy");
 
             app.UseAuthorization();
 
